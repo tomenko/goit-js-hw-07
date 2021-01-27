24ddcae5b86365cbefeb851fa3d5a4fa84111c8a
tomenko <tomenko.sl@gmail.com>
2021-01-27 23:59:57 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -7,27 +7,35 @@
 найдет и выведет в консоль текст заголовка элемента (тега h2) 
 и количество элементов в категории (всех вложенных в него элементов li). */
 
-
+const itemRef = document.querySelectorAll(`.item`);
 const elemRef = document.querySelector('#categories');
 
-console.log(`В списке ${elemRef.children.length} категории.`);
+console.log(`В списке ${itemRef.length} категории.`);
+//console.log(`В списке ${elemRef.children.length} категории.`);
 
 //1-й способ решения
-const categoryArray = document.querySelectorAll(`.item`);
+const getSubList = itemRef.forEach(title => {
+    console.log(`Категория: ${ title.querySelector('h2').textContent }`);
+    console.log(`Количество элементов: ${ title.querySelector('ul').children.length }`);
+});
+
+
+//2-й способ решения
+/* const categoryArray = itemRef;
 for (let elem of categoryArray) {
     console.log(`Категория: ${elem.firstElementChild.textContent}`);
     console.log(`Количество элемментов: ${elem.lastElementChild.children.length}`)
-};
+}; */
 
-//2-й способ решения
+//3-й способ решения
 /* const childrenElem = elemRef.children
 for(let element of childrenElem) {
     console.log(`Категория: ${ title.querySelector('h2').textContent }`);
     console.log(`Количество элементов: ${ title.querySelector('ul').children.length }`)
 }; */
 
-//3-й способ решения
-/* const getSubList = elemRef.forEach((sublist) => {
+//4-й способ решения
+/* const getSubList = itemRef.forEach((sublist) => {
   const title = sublist.children[0].textContent;
   const quantity = sublist.children[1].querySelectorAll("li").length;
 
@@ -35,11 +43,14 @@ for(let element of childrenElem) {
   console.log(`Количество элементов: ${quantity}`);
 }); */
 
-//4-й способ решения
-/* const getSubList = elemRef.forEach (title => {
-    console.log(`Категория: ${ title.querySelector('h2').textContent }`);
-    console.log(`Количество элементов: ${ title.querySelector('ul').children.length }`);
-}); */
+
+
+
+
+
+
+
+
 
 
 
